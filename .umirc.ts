15fb8713e971c65963f141779d7ba9046882c08b
jamesxwang <jameswangx95@gmail.com>
2022-03-12 15:44:21 +0800

    fix(ci): base path and public path on production deployment

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'umi';
 
+const BASE_PATH = '/airwallex-frontend-code-challenge'
+
 export default defineConfig({
   title: 'Broccoli & Co.',
+  base: `${BASE_PATH}`,
+  publicPath: process.env.NODE_ENV === 'production' ? `${BASE_PATH}/` : '/',
   nodeModulesTransform: {
     type: 'none',
   },
