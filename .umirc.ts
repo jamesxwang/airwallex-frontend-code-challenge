587d847eaf3e6bb87cd1cc315e9e0258724e7276
jamesxwang <jameswangx95@gmail.com>
2022-03-14 12:33:52 +0800

    chore: fix base path

--- .umirc.ts
+++ .umirc.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'umi';
 
-const BASE_PATH = '/airwallex-frontend-code-challenge'
+const BASE_PATH = '/airwallex-frontend-code-challenge';
 
 export default defineConfig({
   title: 'Broccoli & Co.',
-  base: `${BASE_PATH}`,
+  base: process.env.NODE_ENV === 'production' ? BASE_PATH : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `${BASE_PATH}/` : '/',
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
 });
